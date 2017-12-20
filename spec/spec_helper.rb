057984ed87b2f212647f6b9095b9c9f735f5275b
Roger Ding <roger.ding@workday.com>
2017-12-20 09:44:12 -0800

    test spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
 require 'simplecov'
 require 'simplecov-json'
 
+module SimpleCov::Configuration
+  def clean_filters
+    @filters = []
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_adapter 'test_frameworks'
+  # add_filter '/Users/roger.ding/.rbenv/'
+end
+
 SimpleCov.start 'test_frameworks'
 
 SimpleCov.formatters = [
